6cb8e6727bc9ab4dd4a51fa908ec473463509a46
Natalie Le <natalie.le@evernorth.com>
2023-09-25 03:40:49 +0000

    changed unit test to pytest instead of unnittest

--- dataframe_comparer/tests/test_comparer.py
+++ dataframe_comparer/tests/test_comparer.py
@@ -1,19 +1,15 @@
-import unittest
 import pandas as pd
 from dataframe_comparer import compare_dataframes
 
-class TestComparer(unittest.TestCase):
-    def test_identical_dfs(self):
-        df1 = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
-        df2 = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
-        result = compare_dataframes(df1, df2)
-        self.assertEqual(result, 'Dataframes are identical.')
+def test_identical_dfs():
+    df1 = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+    df2 = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+    result = compare_dataframes(df1, df2)
+    assert result == 'Dataframes are identical.'
 
-    def test_different_dfs(self):
-        df1 = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
-        df2 = pd.DataFrame({'col1': [1, 2], 'col2': [3, 5]})
-        result = compare_dataframes(df1, df2)
-        self.assertIn((1, 'col2'), result)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_different_dfs():
+    df1 = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+    df2 = pd.DataFrame({'col1': [1, 2], 'col2': [3, 5]})
+    result = compare_dataframes(df1, df2)
+    # check if the difference is in row 1 and column 'col2'
+    assert (1, 'col2') in result
